9ddd531690311a7255c0e7b505266fd70c1ffc41
lai-rui <1094261563@qq.com>
2023-02-07 00:52:38 +0800

    add DOM实战

--- js/DOM实战/pages.js
+++ js/DOM实战/pages.js
@@ -1,3 +1,4 @@
+//生成一个item
 function createItem(no, title, clicktimes, replytimes) {
     var item = document.createElement("div");
     item.setAttribute("class", "item");
@@ -9,23 +10,32 @@ function createItem(no, title, clicktimes, replytimes) {
     item.innerHTML = html;
     return item;
 }
+//生成一个翻页的a链接
+function createPage(pagenum) {
+    var a = document.createElement("a");
+    a.setAttribute("href", "#");
+    a.innerHTML = pagenum + 1;
+    return a;
+}
+//获取父元素dom
 var oList = document.getElementById("list");
-
+var pages = document.getElementById("page")
+//定义item数据
 var data = [];
 for (var i = 0; i < 100; i++) {
     data.push({ no: i, title: "标题" + i, clicktimes: i * 10, replytimes: i * 11 });
 }
-
+//获取一页中所有item数据
 function getDate(pages) {
     return data.slice(pages * 12, pages * 12 + 12);
 }
-
+//将创建的item铺满页面
 function fillDom(pages) {
     //清除当前页面的item
     while (oList.hasChildNodes()) {
         oList.removeChild(oList.lastChild);
     }
-
+    //在list中插入item
     var data = getDate(pages);
     var item = data.map(function (i) {
         return createItem(i.no, i.title, i.clicktimes, i.replytimes);
@@ -35,14 +45,7 @@ function fillDom(pages) {
     })
 
 }
-var pages = document.getElementById("page")
-//动态生成翻页链接
-function createPage(pagenum) {
-    var a = document.createElement("a");
-    a.setAttribute("href", "#");
-    a.innerHTML = pagenum + 1;
-    return a;
-}
+//将翻页铺上画面
 function fillPage() {
     for (var i = 0; i < Math.ceil(data.length / 12); i++) {
         pages.appendChild(createPage(i));
@@ -50,11 +53,11 @@ function fillPage() {
 }
 fillDom(0);
 fillPage();
-
+//翻页响应事件
 var page = document.querySelectorAll(".page a");
 var pagePre = document.getElementById("page_pre");
 var pageNext = document.getElementById("page_next");
-var p = 0;
+var p = 0;//当前页数
 for (let i = 0; i < page.length; i++) {
     page[i].addEventListener("click", function () {
 
@@ -67,6 +70,7 @@ for (let i = 0; i < page.length; i++) {
     })
 
 }
+//左右箭头翻页
 pagePre.onclick = function () {
     for (let i = 0; i < page.length; i++) {
         page[i].className = "";

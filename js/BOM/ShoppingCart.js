6a3ff5eab19f53c6d9aab1e3a8e912ae692b5c53
lai-rui <1094261563@qq.com>
2023-02-11 17:08:05 +0800

    add 浏览器存储

--- js/BOM/ShoppingCart.js
+++ js/BOM/ShoppingCart.js
@@ -1,10 +1,10 @@
 var oCart = document.getElementById("cart");
 var oGoods = document.getElementById("goods");
-var Ginfor = document.querySelectorAll(".goods>.item>.infor");
+
 // 加载商品数据
 var data = [];
 for (var i = 0; i < 5; i++) {
-    data.push({ no: i, num: 0, img: i, title: "商品" + i });
+    data.push({ no: i, num: 1, img: i, title: "商品" + i });
 }
 // 生成商品item
 function createGoods(img, title) {
@@ -26,9 +26,17 @@ function fillGoods() {
         return oGoods.appendChild(i);
     })
 }
+
 fillGoods();
-// 添加商品到购物车
-function addItem(img, title, num) {
+
+//生成购物车数据
+var cartD = [];
+function getCartData(no, img, title, num) {
+    cartD.push({ no: no, num: num, img: img, title: title });
+}
+
+// 生成购物车item
+function createCart(img, title, num) {
     var item = document.createElement("div");
     item.setAttribute("class", "item");
     var html = '<div class="infor">' +
@@ -39,75 +47,74 @@ function addItem(img, title, num) {
     item.innerHTML = html;
     return item;
 }
+// 生成购物车列表
+function fillCart() {
+    //清除当前页面的item
+    while (oCart.hasChildNodes()) {
+        oCart.removeChild(oCart.lastChild);
+    }
+    // 生成item
+    var item = cartD.map(function (i) {
+        return createCart(i.img, i.title, i.num);
+    })
+    item.forEach(function (i) {
+        return oCart.appendChild(i);
+    })
+}
+// 删除购物车数据
+function removeItem(title) {
+    cartD.splice(cartD.findIndex(item => item.title = title), 1);
+}
+
 //响应事件——加入购物车
 var oAdd = document.querySelectorAll(".goods>.item>button");
-var cItem = document.querySelectorAll(".cart>.item");
-var cartGoods = [];
 for (let i = 0; i < oAdd.length; i++) {
     oAdd[i].addEventListener("click", function () {
-        var t = queryCart(data[i].no);
-        console.log(t);
-        if (t[0]) {
-            var dom = cItem[t[1]];
-            // oCart.removeChild(dom);
-            data[i].num++;
-            oCart.replaceChild(addItem(data[i].img, data[i].title, data[i].num), dom)
+        if (cartD.find(item => item.title == data[i].title) != undefined) {
+            // console.log(data[i].title);
+            var j = cartD.findIndex(item => item.title == data[i].title);
+            cartD[j].num++;
         }
         else {
-            cartGoods.push(data[i].no);
-            data[i].num++;
-            oCart.appendChild(addItem(data[i].img, data[i].title, data[i].num));
+            getCartData(data[i].no, data[i].img, data[i].title, data[i].num);
         }
-
-        cItem = document.querySelectorAll(".cart>.item");
+        fillCart();
+        Remove();
+        // oRemv=document.querySelectorAll(".cart>.item>button");
         localStorage.buy = oCart.innerHTML;
-        localStorage.cartGoods = JSON.stringify(cartGoods);
         localStorage.data = JSON.stringify(data);
-        removeCart();
+        localStorage.cartD = JSON.stringify(cartD);
     })
+}
+
+//响应事件——删除
+// var oRemv = document.querySelectorAll(".cart>.item>button");
+function Remove() {
+    var oRemv = document.querySelectorAll(".cart>.item>button");
+    for (let i = 0; i < oRemv.length; i++) {
+        oRemv[i].addEventListener("click", function () {
+            removeItem(cartD[i].no);
+            fillCart();
+            Remove();
+            // oRemv=document.querySelectorAll(".cart>.item>button");
+            localStorage.buy = oCart.innerHTML;
+            localStorage.data = JSON.stringify(data);
+            localStorage.cartD = JSON.stringify(cartD);
+        })
+    }
 }
 
 // localStorage传值
 var CartStorage = localStorage.buy;
-var cartGoodsStorage = localStorage.cartGoods;
 var dataStorage = localStorage.data;
-var cItemStorage = localStorage.cItem;
+var cartDStorage = localStorage.cartD;
 if (CartStorage == undefined)
     oCart.innerHTML = '';
 else {
     oCart.innerHTML = CartStorage;
-    cItem = document.querySelectorAll(".cart>.item");
-}
-if (cartGoodsStorage != undefined)
-    cartGoods = JSON.parse(cartGoodsStorage);
-if (dataStorage != undefined)
     data = JSON.parse(dataStorage);
-
-// 查询购物车是否有相同商品
-function queryCart(goods) {
-    var bool = [];
-    bool[0] = false;
-    for (let i = 0; i < cartGoods.length; i++) {
-        if (cartGoods[i] == goods) {
-            bool[0] = true;
-            bool[1] = i;
-        }
-    }
-    return bool;
+    cartD = JSON.parse(cartDStorage);
 }
+fillCart();
 
-//响应事件——删除
-function removeCart() {
-    var oRemv = document.querySelectorAll(".cart>.item>button");
-    for (let i = 0; i < oRemv.length; i++) {
-        oRemv[i].addEventListener("click", function () {
-            var dom = cItem[i];
-            data[cartGoods[i]].num = 0;
-            cartGoods.splice(i, 1);
-            oCart.removeChild(dom);
-            localStorage.buy = oCart.innerHTML;
-            localStorage.data = JSON.stringify(data);
-            localStorage.cartGoods=JSON.stringify(cartGoods);
-        })
-    }
-}
+Remove();
